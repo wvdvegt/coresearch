873400ceaee60ab20cc51ffb8cb85baedec9b7be
wvdvegt <wim@vander-vegt.nl>
2024-08-02 11:10:37 +0200

    1) Removed program.cs. 2) Added .Net4.5 project with linked sources. 3) Added NuGet packages. 4) Added Query extension mapped onto Get().

---
+++ CoreSearch4/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace coresearch
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// A coresearch extensions.
+    /// </summary>
+    public static class CoreSearchExtensions
+    {
+        #region Methods
+
+        public static List<string> Query(this Coresearch s, string key)
+        {
+            return s.Get(key);
+        }
+
+        #endregion Methods
+    }
+}
